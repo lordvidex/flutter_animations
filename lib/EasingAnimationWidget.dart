d9cb66e2612dc6825d4af6636aa282daa4dc40f7
Lordvidex <lordvidex.prime@gmail.com>
2019-11-16 02:50:43 +0300

    First app commit for the animations project

---
+++ lib/EasingAnimationWidget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class EasingAnimationWidget extends StatefulWidget {
+  @override
+  _EasingAnimationWidgetState createState() => _EasingAnimationWidgetState();
+}
+
+class _EasingAnimationWidgetState extends State<EasingAnimationWidget>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    _animation = Tween(begin: -1, end: 0.0).animate(
+        CurvedAnimation(curve: Curves.fastOutSlowIn, parent: _controller))..addStatusListener(handler);
+  }
+  void handler(status){
+      if(status == AnimationStatus.completed){
+        _animation.removeStatusListener(handler);
+        _controller.reset();
+        _animation = Tween(begin: 0.0,end: 1.0).animate(
+          CurvedAnimation(curve: Curves.fastOutSlowIn,parent: _controller)
+        )..addStatusListener((status){
+          if(status == AnimationStatus.completed){
+            Navigator.pop(context);
+          }
+        });
+        _controller.forward();
+      }
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    //Implemetation of app animation goes here
+    final double width = MediaQuery.of(context).size.width;
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (BuildContext context, Widget child){
+        return Scaffold(
+          body: Transform(
+            transform: Matrix4.translationValues(_animation.value*width, 0, 0),
+            child: new Center(
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.black12,
+              ),
+              ),
+          ),
+        );
+      },
+    );
+  }
+  
+  @override
+  void dispose(){
+    super.dispose();
+    _controller.dispose();
+  }
+}
